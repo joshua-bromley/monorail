9e38d667ead3e58afb0dc080fad491b334b7618b
joshua-bromley <joshua@mattbromley.com>
2017-12-13 10:24:42 -0800

    Started Work on Header File

--- rsc/menu.h
+++ rsc/menu.h
@@ -1,6 +1,22 @@
 #ifndef MENU_H_INCLUDED
 #define MENU_H_INCLUDED
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Button {
+public:
+    Vector2f position;
+    Vector2f dimension;
+    int clicks;
+    Sprite button;
 
 
+    Button(Texture&, Vector2f);
+    bool isClicked(Vector2f);
+    void setPosition(Vector2f);
+    void setSize(Vector2f);
+
+};
 
 #endif // MENU_H_INCLUDED
