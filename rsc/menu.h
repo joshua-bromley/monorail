8ee76b1d36a969a34192a2336709751243718adb
joshua-bromley <joshua@mattbromley.com>
2017-12-14 15:01:58 -0800

    Added Menu Option Class to menu.h

--- rsc/menu.h
+++ rsc/menu.h
@@ -4,19 +4,62 @@
 
 using namespace sf;
 
-class Button {
+class MenuOption{
 public:
     Vector2f position;
     Vector2f dimension;
-    int clicks;
-    Sprite button;
+    Vector2f textureSize;
+    Vector2f texturePosition;
+    int toggles;
+    Sprite option;
 
-
-    Button(Texture&, Vector2f);
-    bool isClicked(Vector2f);
+    MenuOption(Texture&, Vector2f, Vector2f);
     void setPosition(Vector2f);
     void setSize(Vector2f);
+    void setTexture(Texture&);
+    void shiftTexture(Vector2f);
+    Vector2f getPosition();
+    Vector2f getSize();
+    Vector2f getTexturePosition();
+    Vector2f getTextureSize();
+    int getToggles();
+    Sprite getSprite();
+};
 
+MenuOption::MenuOption(Texture& texture, Vector2f sizeInput, Vector2f startPoint){
+    option.setTexture(texture);
+    dimension = sizeInput;
+    textureSize = sizeInput;
+    texturePosition = startPoint;
+    option.setTextureRect(IntRect(texturePosition,textureSize));
+}
+
+void setPosition::MenuOption(Vector2f positionInput){
+    position = positionInput;
+    option.setPosition(position);
+}
+
+void setSize::MenuOption(Vector2f sizeInput){
+    option.setScale(dimension.x/sizeInput.x,dimension.y/sizeInput.y);
+    dimension = sizeInput;
+}
+
+void setTexture::MenuOption(Texture& texture){
+    option.setTexture(texture);
+}
+
+
+
+class Button::MenuOption{
+public:
+    bool isClicked(Vector2f);
+
+
+};
+
+class Dragable::MenuOption{
+public:
+    void dragged(Vector2f);
 };
 
 #endif // MENU_H_INCLUDED
