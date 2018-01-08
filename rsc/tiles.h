5be694968f975ab3eb62e082dbb07b74d86b1a09
Sierra Loiselle <sierraml2001@gmail.com>
2018-01-08 13:43:26 -0800

    tiles.h

---
+++ rsc/tiles.h
@@ -0,0 +1,50 @@
+#ifndef TILES_H_INCLUDED
+#define TILES_H_INCLUDED
+#include <SFML/Graphics.hpp>
+
+class Tiles{
+public:
+    bool placed;
+    int side;
+    int rotation;
+    int end1;
+    int end2;
+    //left = 0, right = 1, up = 2, down = 3
+    Sprite sprite;
+
+    Tiles(Texture & texture,int,int);
+    void ends();
+};
+Tiles::Tiles(Texture & texture,int s,int r){
+    side = s;
+    rotation = r;
+    Sprite.setTexture(Texture);
+    Sprite.setTextureRect(IntRect(rotation*20,side*20,20,20));
+}
+void Tiles::ends(){
+    if(side == 0){
+        if(rotation >= 1){
+            end1 = 0;
+            end2 = 0;
+        }
+        else{
+            end1 = 2;
+            end2 = 3;
+        }
+    }
+    else{
+        if(rotation >=1){
+            end1 = 3;
+        }
+        else{
+            end1 = 2;
+        }
+        if(rotation % 2 == 0){
+            end2 = 0
+        }
+        else{
+            end2 = 1;
+        }
+    }
+}
+ #endif
