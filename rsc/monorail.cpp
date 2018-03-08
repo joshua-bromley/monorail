ce2aec85b9f343e4f443d07bf1c457a36c607f4e
joshua-bromley <joshua@mattbromley.com>
2018-03-07 22:01:32 -0800

    Finished placement and path finding. Turns switch. Added textures for the other buttons.

--- rsc/monorail.cpp
+++ rsc/monorail.cpp
@@ -20,7 +20,9 @@ int main(){
     int tilesPlaced = 0;
     int placedThisTurn = 0;
     Texture texture;
+    Texture buttonTexture;
     texture.loadFromFile("sprites.png");
+    buttonTexture.loadFromFile("MonorailButtons.png");
     vector< vector<Tiles> > tiles;
     for(int i = 0; i < 13; i++){
         vector<Tiles> row;
@@ -29,6 +31,7 @@ int main(){
                 Tiles tiles1(texture, 0, 1, true);
                 tiles1.sprite.setPosition(Vector2f(64*j,64*i));
                 tiles1.ends();
+                tiles1.placed = true;
                 row.push_back(tiles1);
             }
             else{
@@ -41,6 +44,12 @@ int main(){
     }
     Sprite endTurnButton;
     Sprite impossibleButton;
+    endTurnButton.setTexture(buttonTexture);
+    impossibleButton.setTexture(buttonTexture);
+    endTurnButton.setTextureRect(IntRect(0,0,128,128));
+    impossibleButton.setTextureRect(IntRect(128,0,128,128));
+    endTurnButton.setPosition(Vector2f(250,850));
+    impossibleButton.setPosition(Vector2f(750,850));
     Tiles stagingButton(texture);
     stagingButton.sprite.setPosition(10,900);
     Mouse mouse;
@@ -77,7 +86,26 @@ int main(){
                 }
                 stagingButton.retexture(side,rotation);
             }
-            else if(mouse.getPosition(window).y > 0 && mouse.getPosition(window).x > 0 && mouse.getPosition(window).y < 832 && mouse.getPosition(window).x < 896 && tilesPlaced < 16  && placement(tiles,mouse.getPosition(window),placedThisTurn,stagingButton)){
+            else if(endTurnButton.getGlobalBounds().contains(mouse.getPosition(window).x,mouse.getPosition(window).y)){
+                placedThisTurn = 0;
+                turn = !turn;
+                for(int i = 0; i < 13; i++){
+                    for(int j = 0; j < 14; j++){
+                        tiles[i][j].newlyPlaced = false;
+                    }
+                }
+                if(pathfinder(tiles,7,6,0)){
+                    break;
+                }
+                if(turn){
+                    cout << "Player 1s turn" << endl;
+                }
+                else{
+                    cout << "Player 2s turn" << endl;
+                }
+                stagingButton.retexture(side,rotation);
+            }
+            else if(mouse.getPosition(window).y > 0 && mouse.getPosition(window).x > 0 && mouse.getPosition(window).y < 832 && mouse.getPosition(window).x < 896 && tilesPlaced < 16 && placedThisTurn < 3 && placement(tiles,mouse.getPosition(window),placedThisTurn,stagingButton)){
                 tiles[(mouse.getPosition(window).y/64)][(mouse.getPosition(window).x/64)].retexture(side,rotation);
                 tiles[(mouse.getPosition(window).y/64)][(mouse.getPosition(window).x/64)].newlyPlaced = true;
                 tiles[(mouse.getPosition(window).y/64)][(mouse.getPosition(window).x/64)].placed = true;
@@ -85,7 +113,7 @@ int main(){
                 rotation = 0;
                 side = 0;
                 tilesPlaced++;
-                //placedThisTurn++;
+                placedThisTurn++;
                 stagingButton.retexture(side,rotation);
             }
         }
@@ -112,43 +140,52 @@ int main(){
 
 
     }
+    if(turn){
+        cout << "Player 1 wins!" << endl;
+    }
+    else{
+        cout << "Player 2 wins!" << endl;
+    }
 
     return 0;
 }
 
 bool pathfinder(vector< vector<Tiles> >& tiles, int x, int y, int last){
-    if(tiles[x][y].placed == false){
+    if(tiles[y][x].placed == false){
         return false;
     }
-      else if(tiles[x][y].end1 == last){
-        switch(tiles[x][y].end2){
+    else if (x == 6 && y == 6){
+        return true;
+    }
+      else if(tiles[y][x].end1 == last){
+        switch(tiles[y][x].end2){
         case 0:
-            return pathfinder(tiles, x-1 ,y , 1);
+            return pathfinder(tiles, x-1 ,y , 1);//go left
             break;
         case 1:
-            return pathfinder(tiles, x+1, y, 0);
+            return pathfinder(tiles, x+1, y, 0);//go right
             break;
         case 2:
-            return pathfinder(tiles, x, y+1, 3);
+            return pathfinder(tiles, x, y-1, 3);//go down
             break;
         case 3:
-            return pathfinder(tiles, x, y-1, 2);
+            return pathfinder(tiles, x, y+1, 2);//go up
             break;
         }
       }
       else{
-        switch(tiles[x][y].end1){
+        switch(tiles[y][x].end1){
         case 0:
-            return pathfinder(tiles, x-1 ,y , 1);
+            return pathfinder(tiles, x-1 ,y , 1);//go left
             break;
         case 1:
-            return pathfinder(tiles, x+1, y, 0);
+            return pathfinder(tiles, x+1, y, 0);//go right
             break;
         case 2:
-            return pathfinder(tiles, x, y+1, 3);
+            return pathfinder(tiles, x, y-1, 3);//go down
             break;
         case 3:
-            return pathfinder(tiles, x, y-1, 2);
+            return pathfinder(tiles, x, y+1, 2);//go up
             break;
         }
       }
@@ -162,20 +199,17 @@ bool placement(vector< vector<Tiles> >& tiles,Vector2i click, int placedThisTurn
     tiley = click.y/64;
     bool connected = false;
     tempTile.ends();
-    cout << tempTile.end1 << endl << tempTile.end2 << endl;
     if(tilex < 0 || tilex > 14 || tiley < 0 || tiley > 13){
         return false;
     }
     if(tiles[tiley][tilex].placed == true){
-        cout << "A" << endl;
         return false;
         // cannot place
     }
 
         //tile to the right of
-    if(/*placedThisTurn == 0*/1){
+    if(placedThisTurn == 0){
         if(tiles[tiley][tilex + 1].placed == true){
-            cout << "B" << endl;
             if((tiles[tiley][tilex+1].end1 != 0 && tiles[tiley][tilex+1].end2 != 0)&&(tempTile.end1 == 1 || tempTile.end2 == 1)){
                 return false;
             }
@@ -186,7 +220,6 @@ bool placement(vector< vector<Tiles> >& tiles,Vector2i click, int placedThisTurn
         }
         //tile to the left of
         else if(tiles[tiley][tilex - 1].placed == true){
-            cout << "C" << endl;
             if((tiles[tiley][tilex-1].end1 != 1 && tiles[tiley][tilex-1].end2 != 1)&&(tempTile.end1 == 0 || tempTile.end2 == 0)){
                 return false;
             }
@@ -198,27 +231,110 @@ bool placement(vector< vector<Tiles> >& tiles,Vector2i click, int placedThisTurn
 
         // tile above
         else if(tiles[tiley + 1][tilex ].placed == true){
-                cout << "D" << endl;
-            if((tiles[tiley+1][tilex].end1 != 3 && tiles[tiley+1][tilex].end2 != 3)&&(tempTile.end1 == 2 || tempTile.end2 == 2)){
+            if((tiles[tiley+1][tilex].end1 != 2 && tiles[tiley+1][tilex].end2 != 2)&&(tempTile.end1 == 3 || tempTile.end2 == 3)){
                 return false;
             }
-            if((tiles[tiley+1][tilex].end1 == 3 || tiles[tiley+1][tilex].end2 == 3)&&(tempTile.end1 != 2 && tempTile.end2 != 2)){
+            if((tiles[tiley+1][tilex].end1 == 2 || tiles[tiley+1][tilex].end2 == 2)&&(tempTile.end1 != 3 && tempTile.end2 != 3)){
                 return false;
             }
             connected = true;
         }
         //tile below
         else if(tiles[tiley-1][tilex ].placed == true){
-            cout << "E" << endl;
-            if((tiles[tiley-1][tilex].end1 != 2 && tiles[tiley-1][tilex].end2 != 2)&&(tempTile.end1 == 3 || tempTile.end2 == 3)){
+            if((tiles[tiley-1][tilex].end1 != 3 && tiles[tiley-1][tilex].end2 != 3)&&(tempTile.end1 == 2 || tempTile.end2 == 2)){
                 return false;
             }
-            if((tiles[tiley-1][tilex].end1 == 2 || tiles[tiley-1][tilex].end2 == 2)&&(tempTile.end1 != 3 && tempTile.end2 != 3)){
+            if((tiles[tiley-1][tilex].end1 == 3 || tiles[tiley-1][tilex].end2 == 3)&&(tempTile.end1 != 2 && tempTile.end2 != 2)){
                 return false;
             }
             connected = true;
         }
-        cout << connected << endl;
+        return connected;
+    }
+    if(placedThisTurn == 2){
+        if(tiles[tiley][tilex + 1].newlyPlaced == true && tiles[tiley][tilex + 2].newlyPlaced == true){
+            if((tiles[tiley][tilex+1].end1 != 0 && tiles[tiley][tilex+1].end2 != 0)&&(tempTile.end1 == 1 || tempTile.end2 == 1)){
+                return false;
+            }
+            if((tiles[tiley][tilex+1].end1 == 0 || tiles[tiley][tilex+1].end2 == 0)&&(tempTile.end1 != 1 && tempTile.end2 != 1)){
+                return false;
+            }
+            connected = true;
+        }
+        //tile to the left of
+        else if(tiles[tiley][tilex - 1].newlyPlaced == true && tiles[tiley][tilex - 2].newlyPlaced == true){
+            if((tiles[tiley][tilex-1].end1 != 1 && tiles[tiley][tilex-1].end2 != 1)&&(tempTile.end1 == 0 || tempTile.end2 == 0)){
+                return false;
+            }
+            if((tiles[tiley][tilex-1].end1 == 1 || tiles[tiley][tilex-1].end2 == 1)&&(tempTile.end1 != 0 && tempTile.end2 != 0)){
+                return false;
+            }
+            connected = true;
+        }
+
+        // tile above
+        else if(tiles[tiley + 1][tilex ].newlyPlaced == true && tiles[tiley+2][tilex].newlyPlaced == true){
+            if((tiles[tiley+1][tilex].end1 != 2 && tiles[tiley+1][tilex].end2 != 2)&&(tempTile.end1 == 3 || tempTile.end2 == 3)){
+                return false;
+            }
+            if((tiles[tiley+1][tilex].end1 == 2 || tiles[tiley+1][tilex].end2 == 2)&&(tempTile.end1 != 3 && tempTile.end2 != 3)){
+                return false;
+            }
+            connected = true;
+        }
+        //tile below
+        else if(tiles[tiley-1][tilex ].newlyPlaced == true && tiles[tiley-2][tilex].newlyPlaced == true){
+            if((tiles[tiley-1][tilex].end1 != 3 && tiles[tiley-1][tilex].end2 != 3)&&(tempTile.end1 == 2 || tempTile.end2 == 2)){
+                return false;
+            }
+            if((tiles[tiley-1][tilex].end1 == 3 || tiles[tiley-1][tilex].end2 == 3)&&(tempTile.end1 != 2 && tempTile.end2 != 2)){
+                return false;
+            }
+            connected = true;
+        }
+        return connected;
+    }
+    if(placedThisTurn == 1){
+        if(tiles[tiley][tilex + 1].newlyPlaced == true){
+            if((tiles[tiley][tilex+1].end1 != 0 && tiles[tiley][tilex+1].end2 != 0)&&(tempTile.end1 == 1 || tempTile.end2 == 1)){
+                return false;
+            }
+            if((tiles[tiley][tilex+1].end1 == 0 || tiles[tiley][tilex+1].end2 == 0)&&(tempTile.end1 != 1 && tempTile.end2 != 1)){
+                return false;
+            }
+            connected = true;
+        }
+        //tile to the left of
+        else if(tiles[tiley][tilex - 1].newlyPlaced == true){
+            if((tiles[tiley][tilex-1].end1 != 1 && tiles[tiley][tilex-1].end2 != 1)&&(tempTile.end1 == 0 || tempTile.end2 == 0)){
+                return false;
+            }
+            if((tiles[tiley][tilex-1].end1 == 1 || tiles[tiley][tilex-1].end2 == 1)&&(tempTile.end1 != 0 && tempTile.end2 != 0)){
+                return false;
+            }
+            connected = true;
+        }
+
+        // tile above
+        else if(tiles[tiley + 1][tilex ].newlyPlaced == true){
+            if((tiles[tiley+1][tilex].end1 != 2 && tiles[tiley+1][tilex].end2 != 2)&&(tempTile.end1 == 3 || tempTile.end2 == 3)){
+                return false;
+            }
+            if((tiles[tiley+1][tilex].end1 == 2 || tiles[tiley+1][tilex].end2 == 2)&&(tempTile.end1 != 3 && tempTile.end2 != 3)){
+                return false;
+            }
+            connected = true;
+        }
+        //tile below
+        else if(tiles[tiley-1][tilex ].newlyPlaced == true){
+            if((tiles[tiley-1][tilex].end1 != 3 && tiles[tiley-1][tilex].end2 != 3)&&(tempTile.end1 == 2 || tempTile.end2 == 2)){
+                return false;
+            }
+            if((tiles[tiley-1][tilex].end1 == 3 || tiles[tiley-1][tilex].end2 == 3)&&(tempTile.end1 != 2 && tempTile.end2 != 2)){
+                return false;
+            }
+            connected = true;
+        }
         return connected;
     }
 
